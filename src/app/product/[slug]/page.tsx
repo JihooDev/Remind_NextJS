39bf8a74a3ee2e4e3294b183b88f2bfb941a90bf
jihooDev <wlgn829@kakao.com>
2023-10-28 14:18:08 +0900

    feat: staticParams

--- src/app/product/[slug]/page.tsx
+++ src/app/product/[slug]/page.tsx
@@ -7,9 +7,21 @@ type Props = {
 }
 
 const page = ({ params }: Props): ReactElement => {
+
+    console.log(params)
+
+
     return (
         <div>page : {params.slug}</div>
     )
+}
+
+export const generateStaticParams = () => {
+    const blog = ['개발', '일상'];
+
+    return blog.map((item: string) => ({
+        slug: item
+    }))
 }
 
 export default page
