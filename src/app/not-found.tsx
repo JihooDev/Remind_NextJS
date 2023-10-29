14549c86ae47693dfdfa679104700ba12fcd9cbb
jihooDev <wlgn829@kakao.com>
2023-10-29 11:20:18 +0900

    feat: not-found

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+import React, { ReactElement } from 'react'
+
+const NotFound = (): ReactElement => {
+    return (
+        <div>페이지를 찾을 수 없음</div>
+    )
+}
+
+export default NotFound;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image'
 import Button from './components/Button'
+import { notFound } from 'next/navigation'
 
 export default function Home() {
+  // notfound
+  // 1. app/not-fount.tsx 생성
+  // 2. 최 상위 경로에서 notFound() 호출
+  // notFound();
+
   return (
     <div>
       <h1>홈페이지</h1>
